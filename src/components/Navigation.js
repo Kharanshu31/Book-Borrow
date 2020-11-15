b4200707cec352955d81cdc3773c8e2971c61e33
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-11-15 18:34:17 +0530

    Add navigation bar

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <div className="navigationContainer">
+      <div>
+        <h1 className="navigationTitle">Book-Borrow</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
