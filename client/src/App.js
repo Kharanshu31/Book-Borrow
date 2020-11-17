c671e3a9d4f26464f89a03a1fdd888b9cb1c8425
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-11-18 01:18:25 +0530

    Update

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+//import Navigation from "./components/Navigation.js";
+
+function App() {
+  return (
+    <div className="navigationContainer">
+      <div>
+        <h1 className="navigationTitle">Book-Borrow</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
