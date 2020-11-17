d945b27cecefb2747eb19463f497b59be4398eb2
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-11-18 01:21:22 +0530

    Changed directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 
-//import Navigation from "./components/Navigation.js";
+import Navigation from "./components/Navigation.js";
 
 function App() {
   return (
-    <div className="navigationContainer">
-      <div>
-        <h1 className="navigationTitle">Book-Borrow</h1>
-      </div>
+    <div>
+      <Navigation />
     </div>
   );
 }
